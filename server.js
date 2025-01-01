795e0d77612c551537dd6cf7217ea5f68ea2f710
Amin <neroxvero@gmail.com>
2025-01-01 14:41:49 -0500

    working on bugs

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.get('/', async (request, response) => {
 
   try {
 
-    response.render(__dirname + '/views/entry-interface', {
+    response.render(__dirname + '/entry-interface', {
       title: 'Portofolio',
       styles: ['/css/style.css'],
       data: roleData,
